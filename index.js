2d7cf6977e6f86f22ef1cb8bd8d38a1ae175261d
Vladyslav Starodubets <dp181191svn@gmail.com>
2023-01-27 19:33:44 +0100

    add comments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var cnv, ctx, sel;
 function getPoints(a, b) {
   var p, k, i, j, phi;
-  var pts = new Array(2);
+  var pts = new Array(2); // array with two starting points
   for (i = 0; i < pts.length; i++) {
     pts[i] = new Array(2);
   }
-
+  //coordinate values for start points
   for (i = 0; i < 2; i++) {
     pts[0][i] = a[i];
     pts[1][i] = b[i];
@@ -14,7 +14,7 @@ function getPoints(a, b) {
   var n = document.getElementById('mysel').selectedIndex;
 
   for (k = 1; k <= n; k++) {
-    p = new Array(4 * pts.length - 3);
+    p = new Array(4 * pts.length - 3); //a new array to store the coordinates of the curve points in it
     for (i = 0; i < pts.length - 1; i++) {
       for (j = 0; j < 4; j++) {
         p[4 * i + j] = new Array(2);
@@ -24,16 +24,16 @@ function getPoints(a, b) {
         p[4 * i + 1][j] = Math.round((2 * pts[i][j]) / 3 + pts[i + 1][j] / 3);
         p[4 * i + 3][j] = Math.round(pts[i][j] / 3 + (2 * pts[i + 1][j]) / 3);
       }
-      phi = Math.atan2(p[4 * i + 1][1] - p[4 * i + 3][1], p[4 * i + 3][0] - p[4 * i + 1][0]);
+      phi = Math.atan2(p[4 * i + 1][1] - p[4 * i + 3][1], p[4 * i + 3][0] - p[4 * i + 1][0]); //angle between line and coordinate axis
       L = Math.sqrt(
         Math.pow(p[4 * i + 3][0] - p[4 * i + 1][0], 2) +
           Math.pow(p[4 * i + 3][1] - p[4 * i + 1][1], 2),
-      );
+      ); // segment length
 
       p[4 * i + 2][0] = Math.round(p[4 * i + 1][0] + L * Math.cos(phi + Math.PI / 3));
       p[4 * i + 2][1] = Math.round(p[4 * i + 1][1] - L * Math.sin(phi + Math.PI / 3));
     }
-    p[4 * pts.length - 4] = new Array(2);
+    p[4 * pts.length - 4] = new Array(2); // last element in dot array
     for (j = 0; j < 2; j++) {
       p[4 * pts.length - 4][j] = pts[pts.length - 1][j];
     }
@@ -41,6 +41,7 @@ function getPoints(a, b) {
   }
   return pts;
 }
+// function to display the curve in the graphics area
 function drawPoints(p, ct) {
   ct.beginPath();
   ct.strokeStyle = 'blue';
@@ -51,7 +52,7 @@ function drawPoints(p, ct) {
   }
   ct.stroke();
 }
-
+// element loading event handler
 window.onload = function () {
   cnv = document.getElementById('mycanvas');
   ctx = cnv.getContext('2d');
@@ -64,7 +65,8 @@ window.onload = function () {
     sel.selectedIndex = 0;
   }
 
-  drawPoints(getPoints([10, cnv.height - 10], [cnv.width - 10, cnv.height - 10]), ctx);
+  drawPoints(getPoints([10, cnv.height - 10], [cnv.width - 10, cnv.height - 10]), ctx); // curve display
+  // event handler for the dropdown state change event
   sel.onchange = function () {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
     drawPoints(getPoints([10, cnv.height - 10], [cnv.width - 10, cnv.height - 10]), ctx);
